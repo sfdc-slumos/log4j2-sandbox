020ebba07980399ab3eaf085794bf50e7f6acae3
Steven Lumos <slumos@salesforce.com>
2019-10-18 14:56:33 -0700

    figure out basics of logging and .properties configuration

--- src/main/java/daggerok/App.java
+++ src/main/java/daggerok/App.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class App {
   public static void main(String[] args) {
-    log.err("yo!");
+    log.debug("DEBUG");
+    log.info("INFO");
+    log.warn("WARN");
+    log.error("ERORR");
   }
 }
